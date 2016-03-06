2cf1f8f101fa7a518d5f8f46108184eed89bf047
Fernando Rueda <nanr3d@iMac-de-Fernando-2.local>
2016-03-06 16:42:51 +0100

    Set namespace and change method name

--- ViewVarsRecollector.php
+++ ViewVarsRecollector.php
@@ -1,6 +1,6 @@
 <?php 
    
-namespace ViewVarsRecollector;
+namespace Marel\Tools;
 
 class ViewVarsRecollector {
 	
@@ -8,21 +8,21 @@ class ViewVarsRecollector {
 
 	public function __construct($settings)
 	{
-		$this->$prefix = array_key_exists('prefix', $settings) ? $settings['prefix'] : "_"; // _ por defecto
+		$this->$prefix = array_key_exists('prefix', $settings) ? $settings['prefix'] : "_"; // _ by default
 	}
 
-	public function filter($all_vars)
+	public function filterVars($all_vars)
 	{
-	    $vars_to_view = array_filter($all_vars, function($variable_content, $variable_name) {
-	        // True cuando el nombre de la variable empieze por $this->$prefix
+	    $vars_to_parse = array_filter($all_vars, function($variable_content, $variable_name) {
+	        // True when variable name begin with $this->$prefix
 	        return strpos($variable_name, $this->$prefix) === 0;
 	    }, ARRAY_FILTER_USE_BOTH);
 
-	    $data_to_view = [];
-	    foreach($vars_to_view as $nombre => $contenido) {
-	        $data_to_view[ltrim($nombre, $this->$prefix)] = $contenido;
+	    $vars_to_view = [];
+	    foreach($vars_to_parse as $nombre => $contenido) {
+	        $vars_to_view[ltrim($nombre, $this->$prefix)] = $contenido;
 	    }
-		return $data_to_view;
+		return $vars_to_view;
 	}
 
 }
